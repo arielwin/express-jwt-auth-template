1ad950eb6c56a128fbf474d993f7088c55957c03
arielwin <subiboy7@gmail.com>
2024-08-02 13:49:01 -0600

    completed template

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,28 @@
-const NavBar = ({ user }) => {
-    return (
-      <>
-        <p>Hello Navbar</p>
-      </>
-    )
-  }
-  export default NavBar
-  
+import { Link } from 'react-router-dom';
+
+// function component
+
+// intakes props
+const NavBar = ({ user, handleSignout }) => {
+  return (
+    <>
+      { user ? (
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+          </ul>
+        </nav>
+      ) : (
+        <nav>
+          <ul>
+            <li><Link to="/signin">Sign In</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+          </ul>
+        </nav>
+      )}
+    </>
+  )
+}
+
+export default NavBar;
